a8f8bbc89d2a166131ddb80c6b6d8c104292c903
xionglongbing <2548898737@qq.com>
2024-09-21 23:31:18 +0800

    打包改为相对路径

--- vite.config.js
+++ vite.config.js
@@ -11,51 +11,52 @@ import IconsResolver from 'unplugin-icons/resolver';
 import Inspect from 'vite-plugin-inspect';
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        Vue(),
-        AutoImport({
-            // Auto import functions from Vue, e.g. ref, reactive, toRef...
-            // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
-            imports: ['vue'],
-            // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
-            // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
-            resolvers: [
-                ElementPlusResolver(),
-                // Auto import icon components
-                // 自动导入图标组件
-                IconsResolver({
-                    prefix: 'Icon'
-                })
-            ],
-            dts: path.resolve(pathSrc, 'auto-imports.d.ts')
+  base: './',
+  plugins: [
+    Vue(),
+    AutoImport({
+      // Auto import functions from Vue, e.g. ref, reactive, toRef...
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue'],
+      // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [
+        ElementPlusResolver(),
+        // Auto import icon components
+        // 自动导入图标组件
+        IconsResolver({
+          prefix: 'Icon'
+        })
+      ],
+      dts: path.resolve(pathSrc, 'auto-imports.d.ts')
+    }),
+    Components({
+      resolvers: [
+        // Auto register icon components
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep']
         }),
-        Components({
-            resolvers: [
-                // Auto register icon components
-                // 自动注册图标组件
-                IconsResolver({
-                    enabledCollections: ['ep']
-                }),
-                // Auto register Element Plus components
-                // 自动导入 Element Plus 组件
-                ElementPlusResolver()
-            ],
-            dts: path.resolve(pathSrc, 'components.d.ts')
-        }),
-        Icons({
-            autoInstall: true
-        }),
-        Inspect()
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-            '@/components': path.resolve(__dirname, 'src/components')
-        },
-        // 省略后缀
-        extensions: ['.js', '.ts', '.vue', '.json']
+        // Auto register Element Plus components
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver()
+      ],
+      dts: path.resolve(pathSrc, 'components.d.ts')
+    }),
+    Icons({
+      autoInstall: true
+    }),
+    Inspect()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@/components': path.resolve(__dirname, 'src/components')
     },
-    build: {
-        chunkSizeWarningLimit: 1000 // 设置为1000kB
-    }
+    // 省略后缀
+    extensions: ['.js', '.ts', '.vue', '.json']
+  },
+  build: {
+    chunkSizeWarningLimit: 1000 // 设置为1000kB
+  }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ import IconsResolver from 'unplugin-icons/resolver';
 import Inspect from 'vite-plugin-inspect';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/wonderfulJourney/',
+  base: './',
   plugins: [
     Vue(),
     AutoImport({
