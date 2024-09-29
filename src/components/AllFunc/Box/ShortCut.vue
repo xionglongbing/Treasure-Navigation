e6d78b04087c935eb052543341f37aafa2e1edde
xionglongbing <2548898737@qq.com>
2024-09-29 22:45:00 +0800

    调整布局，和加上注释

--- src/components/AllFunc/Box/ShortCut.vue
+++ src/components/AllFunc/Box/ShortCut.vue
@@ -1,7 +1,7 @@
 <template>
   <Transition name="fade" mode="out-in">
-    <div v-if="categoryDataList.length" class="shortcut">
-      <n-scrollbar class="box__padding--lr20">
+    <div v-if="categoryDataList.length" class="shortcut__content box__padding--lr20">
+      <n-scrollbar>
         <!-- <template v-for="categoryData in categoryDataList" :key="categoryData.categoryName"> -->
         <VueDraggable :animation="150" v-model="categoryDataList">
           <n-collapse
@@ -76,24 +76,24 @@
           </n-collapse>
           <!-- </template> -->
         </VueDraggable>
-        <div class="footer__btn-group">
-          <div class="footer__btn-group--left">
-            <div class="footer__btn" @click="downloadHtmlFile">
-              <SvgIcon iconName="icon-xiazai" />
-              <span class="name">下载</span>
-            </div>
-            <div class="footer__btn" @click="clickFileDom">
-              <input type="file" name="上传" id="shortCutUploadInput" />
-              <SvgIcon iconName="icon-shangchuan" />
-              <span class="name">上传</span>
-            </div>
+      </n-scrollbar>
+      <div class="footer__btn-group">
+        <div class="footer__btn-group--left">
+          <div class="footer__btn" @click="downloadHtmlFile">
+            <SvgIcon iconName="icon-xiazai" />
+            <span class="name">下载</span>
           </div>
-          <div class="shortcut__add" @click="openAddShortcutModal">
-            <SvgIcon iconName="icon-add" />
-            <span class="name">添加导航</span>
+          <div class="footer__btn" @click="clickFileDom">
+            <input type="file" name="上传" id="shortCutUploadInput" />
+            <SvgIcon iconName="icon-shangchuan" />
+            <span class="name">上传</span>
           </div>
         </div>
-      </n-scrollbar>
+        <div class="shortcut__add" @click="openAddShortcutModal">
+          <SvgIcon iconName="icon-add" />
+          <span class="name">添加导航</span>
+        </div>
+      </div>
     </div>
     <div v-else class="not-shortcut">
       <span class="tip">暂无导航，去添加吧</span>
@@ -243,63 +243,86 @@ function messageTip(messageInfo: MessageInfo) {
 const isAddShortcutModalVisible = ref(false);
 const isEditMode = ref(false);
 
+// 定义一个常量，存储 script 标签的 id
 const scriptDataId: string = 'json-script';
 // 下载导航文件
 function downloadHtmlFile() {
+  // 定义一个包含 HTML 内容的字符串
   const htmlStr: string = `
-    <!DOCTYPE html>
-    <html lang="en">
+    <!DOCTYPE html> <!-- 声明文档类型为 HTML -->
+    <html lang="en"> <!-- 设置语言为英语 -->
 
       <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>导航集合</title>
+        <meta charset="UTF-8"> <!-- 设置字符编码为 UTF-8 -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- 适配不同设备的视口设置 -->
+        <title>导航集合</title> <!-- 网页标题 -->
       </head>
 
       <body>
-      </body>
-      <script id="${scriptDataId}" type="application/json">
-        ${JSON.stringify(site.categoryDataList)}
+      </body> <!-- 网页主体部分，当前为空 -->
+      <script id="${scriptDataId}" type="application/json"> <!-- 创建一个 script 标签，id 为 json-script，内容为 JSON 格式的数据 -->
+        ${JSON.stringify(site.categoryDataList)} <!-- 将 categoryDataList 转换为 JSON 字符串并插入到 script 标签中 -->
       <\/script>
-      <script>
-        const jsonDom = document.querySelector("#${scriptDataId}");
-        const jsonStr = jsonDom.innerText;
-        const jsonData = JSON.parse(jsonStr);
-        console.log('jsonData', jsonData);
+      <script> <!-- 另一个 script 标签，用于处理 JSON 数据 -->
+        // 获取刚刚插入的 script 标签
+        const jsonDom = document.querySelector("#${scriptDataId}"); 
+        // 获取 script 标签中的文本内容（JSON 字符串）
+        const jsonStr = jsonDom.innerText; 
+        // 将 JSON 字符串解析为 JavaScript 对象
+        const jsonData = JSON.parse(jsonStr); 
+        // 打印解析后的数据到控制台
+        console.log('jsonData', jsonData); 
+        
+        // 获取网页的主体部分
         const bodyDom = document.querySelector("body");
+        
+        // 定义一个函数，用于生成网站链接的 HTML
         function getWebsiteDataListDom(websiteDataList) {
-          let htmlStr = "";
+          // 初始化一个空字符串，用于存放生成的 HTML
+          let htmlStr = ""; 
+          // 遍历传入的每个网站数据项
           websiteDataList.forEach(item => {
+            // 为每个网站生成一个列表项的 HTML，并拼接到 htmlStr 中
             htmlStr +=
               \`
                 <dd><a href="\${item.url}" target="_blank">\${item.name}</a></dd>
               \`
-          })
+          });
+          // 返回生成的 HTML 字符串
           return htmlStr;
         }
+        
+        // 遍历解析后的数据
         jsonData.forEach(item => {
+          // 将每个分类和对应的网站链接添加到网页主体中
           bodyDom.innerHTML +=
             \`
               <dl>
                 <dt>
-                  <h2>\${item.categoryName}</h2>
+                  <h2>\${item.categoryName}</h2> <!-- 分类名称 -->
                 </dt>
-                \${getWebsiteDataListDom(item.websiteDataList)}
+                \${getWebsiteDataListDom(item.websiteDataList)} <!-- 获取该分类下的网站链接 -->
               </dl>
             \`
         });
-      <\/script>
+      <\/script> <!-- 结束脚本 -->
     <\/html>
   `;
+
+  // 创建一个 Blob 对象，将 HTML 字符串封装成一个可下载的文件
   const htmlStrBolo = new Blob([htmlStr], { type: 'text/html' });
-  console.log('htmlStrBolo', htmlStrBolo);
+  // 创建一个指向 Blob 对象的 URL
   const htmlStrUrl = URL.createObjectURL(htmlStrBolo);
-  console.log('htmlStr', htmlStr);
-  console.log('htmlStrUrl', htmlStrUrl);
+
+  // 创建一个链接元素
   const aLink = document.createElement('a');
+  // 设置链接的 href 属性为刚刚生成的 URL
   aLink.href = htmlStrUrl;
+  // 设置下载文件的名称
   aLink.download = '导航下载文件.html';
+  // 触发链接的点击事件，开始下载
   aLink.click();
+  // 释放之前创建的 URL 对象，释放内存
   URL.revokeObjectURL(htmlStrUrl);
 }
 
@@ -498,9 +521,11 @@ function openWebsite(url: string) {
 </script>
 
 <style lang="scss" scoped>
-.shortcut {
+.shortcut__content {
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
   :deep(.n-collapse) .n-collapse-item__header > .n-collapse-item__header-main {
     padding: 10px;
     border-radius: 8px;
